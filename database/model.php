2bc188f209f44644ad135f016ab5c89de74d51f5
Kvidera <ed4481hu@winona.edu>
2018-04-21 16:18:37 -0500

    Files missed from last commit

--- database/model.php
+++ database/model.php
@@ -1,53 +1,86 @@
 <?php
-class DatabaseConnection {
+
+class DatabaseConnection
+{
     protected $db;
 
-    public function __construct(PDO $db) {
+    public function __construct(PDO $db)
+    {
         $this->db = $db;
     }
 
-    public function getApprovedGrowers() {
+    public function getApprovedGrowers()
+    {
         return $this->db->query('SELECT * FROM approved_growers');
     }
 
-    public function getSubmittedGrowers() {
+    public function getSubmittedGrowers()
+    {
         return $this->db->query('SELECT * FROM submitted_growers');
     }
 
-    public function getApprovedCoffees() {
+    public function getApprovedCoffees()
+    {
         return $this->db->query('SELECT * FROM approved_coffees');
     }
 
-    public function getSubmittedCoffees() {
+    public function getSubmittedCoffees()
+    {
         return $this->db->query('SELECT * FROM submitted_coffees');
     }
 
-    public function getStores() {
+    public function getCoffeesByStoreID($StoreID)
+    {
+        $res = $this->db->prepare(
+            'select * from coffees co, carries ca, stores s WHERE co.Coffee_ID = ca.Coffee_ID ' .
+            'AND s.Store_ID = ca.Store_ID AND s.Store_ID = :ID; ');
+        $res->execute(['ID' => $StoreID]);
+        return $res->fetchAll();
+    }
+
+    public function getStores()
+    {
         return $this->db->query('SELECT * FROM Stores');
     }
 
-    public function getCoffeeByID($ID) {
-        $res = $this->db->prepare("SELECT * FROM submitted_coffees WHERE Coffee_ID = :ID");
+    public function getStoreByID($ID)
+    {
+        $res = $this->db->prepare("SELECT * from stores WHERE Store_ID = :ID");
         $res->execute(['ID' => $ID]);
         return $res->fetch();
     }
 
-    public function getGrowerByID($ID) {
+    public function getCoffeeByID($ID)
+    {
+        $res = $this->db->prepare("SELECT * FROM submitted_coffees WHERE Coffee_ID = :ID");
+        $res->execute(['ID' => $ID]);
+        if ($res->rowCount() == 0) {
+            return false;
+        } else {
+            return $res->fetch();
+        }
+    }
+
+    public function getGrowerByID($ID)
+    {
         $res = $this->db->prepare("SELECT * FROM submitted_growers WHERE Grower_ID = :ID");
         $res->execute(['ID' => $ID]);
         return $res->fetch();
     }
-	
-	public function submitNewGrower($firstName, $lastName, $location, $farmName, $shortDesc, $longDesc, $approved) {
-		$res = $this->db->prepare(
-			"INSERT INTO Growers (First_Name, Last_Name, Location, Farm_Name, Short_Desc, Long_Desc) VALUES ".
-			"(:firstName, :lastName, :location, :farmName, :shortDesc, :longDesc, :approved)");
-		$res->execute(['firstName' => $firstName, 'lastName' => $lastName, 'location' => $location, 'farmName' => $farmName,
-			'shortDesc' => $shortDesc, 'longDesc' => $longDesc, 'approved' => $approved]);
-	}
-	
-	public function approveGrower($ID) {
-		$res = $this->db->prepare("UPDATE Growers SET Approved = 1 WHERE Grower_ID = :ID");
-		$res->execute(['ID' => $ID]);
-	}
+
+    public function submitNewGrower($firstName, $lastName, $location, $farmName, $shortDesc, $longDesc, $approved)
+    {
+        $res = $this->db->prepare(
+            "INSERT INTO Growers (First_Name, Last_Name, Location, Farm_Name, Short_Desc, Long_Desc) VALUES " .
+            "(:firstName, :lastName, :location, :farmName, :shortDesc, :longDesc, :approved)");
+        $res->execute(['firstName' => $firstName, 'lastName' => $lastName, 'location' => $location, 'farmName' => $farmName,
+            'shortDesc' => $shortDesc, 'longDesc' => $longDesc, 'approved' => $approved]);
+    }
+
+    public function approveGrower($ID)
+    {
+        $res = $this->db->prepare("UPDATE Growers SET Approved = 1 WHERE Grower_ID = :ID");
+        $res->execute(['ID' => $ID]);
+    }
+
 }
